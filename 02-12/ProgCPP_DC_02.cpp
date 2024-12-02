960f224baf038fbf8d5f05fad9f61cd386808cdc
David Concha <david.concha91@gmail.com>
2024-12-02 16:42:53 +0000

    Age and Height

---
+++ 02-12/ProgCPP_DC_02.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int idade;
+    float altura;
+    cout << "Introduza a sua idade: ";
+    cin >> idade;
+
+    cout << "Introduza a sua altura: ";
+    cin >> altura;
+
+    cout << "A sua idade: " << idade << " e a sua altura: " << altura << endl;    
+    
+    return 0;
+    
+}
